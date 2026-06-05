aa1c8ed7c737237d49b0d93d4188f410d05f4d7f
yus <swiftcase@gmail.com>
2026-06-06 02:45:58 +0800

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,18 +1,19 @@
+// preload.js
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
     // Audio processing
-    processAudio: (buffer, params) => ipcRenderer.invoke('process-audio', buffer, params),
+    processAudio: (audioArray, params) => ipcRenderer.invoke('process-audio', audioArray, params),
+    
+    // Transfer function points for plot
     getTransferPoints: (params) => ipcRenderer.invoke('get-transfer-points', params),
     
-    // File operations
-    saveFile: (data, filename) => ipcRenderer.invoke('save-file', data, filename),
+    // File saving
+    saveFile: (data, filename) => ipcRenderer.invoke('save-file', { data, filename }),
     
     // Menu events
     onMenuOpenFile: (callback) => ipcRenderer.on('menu-open-file', callback),
     onMenuExport: (callback) => ipcRenderer.on('menu-export', callback),
-    onModeSwitch: (callback) => ipcRenderer.on('mode-switch', callback),
-    
-    // Remove listeners
-    removeAllListeners: (channel) => ipcRenderer.removeAllListeners(channel)
+    onModeSwitch: (callback) => ipcRenderer.on('mode-switch', callback)
 });
+
